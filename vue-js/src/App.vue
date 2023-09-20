b7d8805af7d697787e4ec1bfaaf065fe255d0d78
Martin Castro <devmcastro02@gmail.com>
2023-09-20 12:59:07 -0700

    Login usando JSON, Fetch y Map - Trabajo #12

--- vue-js/src/App.vue
+++ vue-js/src/App.vue
@@ -1,49 +1,47 @@
 <script setup>
 import { ref } from 'vue'
 
-let name = ref("");
-let lastname = ref("");
-let persons = ref([]);
+const email = ref('')
+const password = ref('')
+const users = ref([])
+const loginError = ref(false)
+const logged = ref(false)
 
-function savePerson() {
-  console.log("hola");
-
-  let person = {
-    firstName: name.value,
-    lastName:  lastname.value
-  }
-
-  console.log(person);
-
-  persons.value.push(person);
+const getUsers = async () => {
+    const response = await fetch('src/users.json')
+    users.value = await response.json()
 }
 
+getUsers()
 
+const handleSubmit = () => {
+  const usersMap = new Map(users.value.map(user => [user.email, user]));
+  const user = usersMap.get(email.value);
 
+  if (user && user.password === password.value) {
+    logged.value = true
+    loginError.value = false
+  } else {
+    loginError.value = true
+    logged.value = false
+  }
+}
 </script>
---------------------------
+
 <template>
+  <div>
+    <h1>Inicio de sesión</h1>
+    <form action="POST">
+      <label for="email">Email:</label>
+      <input type="email" v-model="email" />
 
-  <form action="">
+      <label for="password">Password:</label>
+      <input type="password" v-model="password" />
 
-    <fieldset>
-      <legend>Nombre</legend>
-      <input placeholder="Write here..." v-model="name" type="text" name="" id="">
-    </fieldset>
-
-    <fieldset>
-      <legend>Lastname</legend>
-      <input placeholder="Write here..." v-model="lastname" type="text" name="" id="">
-    </fieldset>
-
-    <button v-on:click="savePerson" type="button">Save Data</button>
-  </form>
-
-  <ol>
-    <li v-for="p in persons">
-      {{p.firstName}} {{p.lastName}}
-    </li>
-  </ol>
-
-
+      <button @click="handleSubmit" type="button">Ingresar</button>
+    </form>
+    <p v-if="loginError" class="error">El usuario no existe</p>
+    <p v-if="logged" class="success">Inicio de sesión exitoso</p>
+  </div>
 </template>
+
